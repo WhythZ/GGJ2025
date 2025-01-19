3476eb7a80e2a7a0c8cdca23fa4f19934a9b5eca
WhythZ <jichenzong@outlook.com>
2025-01-20 00:32:03 +0800

    01.20 fix coyote time bug

--- Assets/Scripts/Crab/States/CrabStateFall.cs
+++ Assets/Scripts/Crab/States/CrabStateFall.cs
@@ -26,8 +26,11 @@ public class CrabStateFall : CrabState
         base.OnUpdate();
 
         //土狼时间内可以进行跳跃，以优化手感
-        if (Input.GetKeyDown(KeyCode.Space) && stateTimer >= 0)
-            stateMachine.ChangeState(crab.jumpState);
+        if (crab.stateMachine.formerState == crab.idleState || crab.stateMachine.formerState == crab.moveState)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && stateTimer >= 0)
+                stateMachine.ChangeState(crab.jumpState);
+        }
 
         //若在地面或泡泡上，则转化为站立状态
         if (crab.isGround || crab.isBubble)
